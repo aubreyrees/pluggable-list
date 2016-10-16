6c8b3d5b5ed29ec49087b7eaa25ba675021b59e1
Aubrey Stark-Toller <aubrey@deepearth.uk>
2016-10-16 02:45:52 +0100

    Add license info to file

--- pluggable_list/utils/iter_tools.py
+++ pluggable_list/utils/iter_tools.py
@@ -1,6 +1,23 @@
 """
 Functions to help with various iteration operations.
+
+Copyright (C) 2016 Aubrey Stark-Toller <aubrey@deepearth.uk>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+
 import functools
 from ..constants import REGISTER_ATTR
 from ..exceptions import ToFewValues, ToManyValues
